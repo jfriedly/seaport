9d87179fbe13282efa37728c908a7083339c5478
Joel Friedly <joelfriedly@gmail.com>
2019-02-19 14:48:18 -0800

    Log $PORT to figure out the value on Heroku

--- web_server.py
+++ web_server.py
@@ -1,4 +1,12 @@
+import logging
+import os
+
 from flask import Flask
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.debug("$PORT is " + os.getenv("PORT"))
+
 app = Flask(__name__)
 
 @app.route("/")
