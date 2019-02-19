f30548496780a34d3aba4dbcd0ddc25e51ae41b2
Joel Friedly <joelfriedly@gmail.com>
2019-02-19 15:56:22 -0800

    Return a static fleet recommendation on form submits

--- web_server.py
+++ web_server.py
@@ -3,6 +3,8 @@ import os
 
 import flask
 from flask import Flask
+from flask import request
+
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -10,6 +12,21 @@ logger.debug("$PORT is " + os.getenv("PORT"))
 
 app = Flask(__name__)
 
-@app.route("/")
+
+class Recommendation(object):
+    requested_capacity = 0
+    recommended_capacity = 0
+    fleet = []
+
+    def __init__(self, requested_capacity, fleet):
+        self.requested_capacity = requested_capacity
+        self.recommended_capacity = sum(fleet)
+        self.fleet = fleet
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return flask.render_template('index.html')
+    if request.method == 'GET':
+        return flask.render_template('index.html')
+    else:
+        flask.g.recommendation = Recommendation(42, [21, 21])
+        return flask.render_template('index.html')
